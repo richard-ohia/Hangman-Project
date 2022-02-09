53ffa0ee6377f81b28cab67bc03e7f0b97db6e74
richard-ohia <rohia@bowdoin.edu>
2022-02-09 17:57:32 -0500

    Full Program

--- hangman.py
+++ hangman.py
@@ -1,155 +1,181 @@
+from random import randrange
 from tkinter.font import ITALIC
 import words
 from tkinter import *
+import pygame
+class Hangman:
+    def __init__(self):
+        self.window = Tk()
+        pygame.mixer.init()
+        self.word_list = list()
+        self.ingame_word = list("_" * len(self.word_list))
+        self.checkSpace()
+        self.buttons = []
+        self.lives = 6 #This is for each body part of the hangman
+        self.musicOn = False
+        self.photo_image = PhotoImage()
+        self.music_button = Button()
+        self.canvas = Canvas()
+        self.label1 = Label()
+        self.label2 = Label()
+        
+        self.window.title("Hangman")
+        self.window.geometry("900x700") #widthxheight
+        self.window.config(bg="#323231")
+        header = Label(self.window, text="H_NGM_N", bg="#323231", fg="#C1436D", font=("Helvetica",70,ITALIC))
+        header.place(relx=.48,rely=.1,anchor=CENTER)
+        
 
-window = Tk()
-word_list = list(words.game_word())
-finished = False #Keeps track of when game is completed
-ingame_word = list("_" * len(word_list))
-guessed_letters = set()
-buttons = []
+    def checkSpace(self):
+        index = 0
+        for i in self.word_list:
+            if i == " ":
+                self.ingame_word[index] = " "
+            elif i == "'":
+                self.ingame_word[index] = "'"
+            index += 1
 
-def play(index, user_input):
-    lives = 6 #This is for each body part of the hangman Please choose a letter:
-    disable(index - 65, user_input) 
-    print(word_list)
-    print(ingame_word)
-    if(user_input in guessed_letters):
-        print("\nYou guessed this letter already. Try again!")
-        label1 = Label(window, text=" ".join(ingame_word), bg="#323231",fg="#C1436D",font=("Arial",20))
-        label1.place(relx=.6,rely=.5,x=900/2 - 250,anchor=CENTER)
-    elif(user_input in word_list and len(user_input) == 1):
-        print("\nNice " + '"' + user_input + '"' + " is part of the word.")
-        guessed_letters.add(user_input)
-        indices = [i for i, value in enumerate(word_list) if value == user_input] ## Wtf is this
-        for index in indices:
-            ingame_word[index] = user_input
-        if("_" not in ingame_word):
-            finished = True
-            print("\nYou Won! The word was " + "".join(ingame_word) + ".")
+    def gameWord(self, choice):
+        if choice == "Random Word":
+            self.word_list = list(words.game_word("words.txt"))
+        elif choice == "Cities":
+            self.word_list = list(words.game_word("cities.txt"))
+        elif choice == "Countries":
+            self.word_list = list(words.game_word("countries.txt"))
+        elif choice == "States":
+            self.word_list = list(words.game_word("states.txt"))
+
+
+        
+
+    def play(self,index, user_input):
+        self.disable(index, user_input)
+        print(self.word_list)
+        print(self.ingame_word)
+        if(user_input in self.word_list and len(user_input) == 1):
+            count = 0
+            for i in self.word_list:
+                if i == user_input:
+                    self.ingame_word[count] = user_input
+                count += 1
+            if("_" not in self.ingame_word):
+                self.display(self.lives)
+                self.gameWon(True)
+            else:
+                self.display(self.lives)
+        elif(user_input not in self.word_list and len(user_input) == 1):
+            self.lives -= 1
+            self.display(self.lives)
+        if self.lives <= 0:
+            self.gameWon(False)
+
+
+    def disable(self,index, n):
+        self.buttons[index].config(state="disabled")
+
+    def trueAnswer(self):
+        self.label1 = Label(self.window, text=" ".join(self.word_list), bg="#323231",fg="#C1436D",font=("Arial",20))
+        self.label1.place(relx=.65,rely=.5,x=900/2 - 250,anchor=CENTER)
+
+    def gameWon(self, gameFinished):
+        self.canvas.destroy()
+        self.label2.destroy()
+        self.label1.destroy()
+        self.trueAnswer()
+        canvas = Canvas(self.window, bg="#323231", highlightthickness=0)
+        canvas.place(relx=.5,rely=.6,anchor=S)
+        if gameFinished == True:
+            label = Label(canvas, text="🥳", bg="#323231",fg="#C1436D",font=("Arial",200))
         else:
-            print(display(lives))
-            label1 = Label(window, text=" ".join(ingame_word), bg="#323231",fg="#C1436D",font=("Arial",20))
-            label1.place(relx=.6,rely=.5,x=900/2 - 250,anchor=CENTER)
-    elif(user_input not in word_list and len(user_input) == 1):
-        print('\n"' + user_input + '"' + " is not part of the word.")
-        guessed_letters.add(user_input)
-        lives -= 1
-        print(display(lives))
-        label1 = Label(window, text=" ".join(ingame_word), bg="#323231",fg="#C1436D",font=("Arial",20))
-        label1.place(relx=.6,rely=.5,x=900/2 - 250,anchor=CENTER)
-        print("\nYou have " + str(lives) + " lives.")
-    else:
-        print("\nPlease input one letter. Try again!")
+            label = Label(canvas, text="🥲", bg="#323231",fg="#C1436D",font=("Arial",200))
+        label.pack()
+        button = Button(canvas, text = "Play Again", fg="Black", highlightbackground="#323231", highlightthickness=0, width=10, height=2, command= lambda : self.restart())
+        button2 = Button(canvas, text = "End Game", fg="Black", highlightbackground="#323231", highlightthickness=0, width=10, height=2, command= lambda : self.endGame())
+        button.pack()
+        button2.pack()
 
-def disable(index, n):
-    print(n)
-    buttons[index].config(state="disabled")
+    def restart(self):
+        if self.musicOn == True:
+            self.stopMusic()
+        self.window.destroy()
+        game = Hangman()
+        game.main()
+
+    def endGame(self):
+        self.window.destroy()
 
 
-def display(lives):
-    stages = [  # final state: head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # head, torso, both arms, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     /
-                   -
-                """,
-                # head, torso, and both arms
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |      
-                   -
-                """,
-                # head, torso, and one arm
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |    
-                   -
-                """,
-                # head and torso
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |    
-                   -
-                """,
-                # head
-                """
-                   --------
-                   |      |
-                   |      O
-                   |    
-                   |      
-                   |    
-                   -
-                """,
-                # initial empty state
-                """
-                   --------
-                   |      |
-                   |      
-                   |    
-                   |      
-                   |    
-                   -
-                """
-    ]
-    return stages[lives]
 
-def main():
-    window.title("Hangman")
-    window.geometry("900x700") #widthxheight
-    window.config(bg="#323231")
-    header = Label(window, text="H_NGM_N", bg="#323231", fg="#C1436D", font=("Helvetica",70,ITALIC))
-    header.place(relx=.46,rely=.1,anchor=CENTER)
-    
-    photo_image = PhotoImage(file="hangman0.png")
-    larger_image = photo_image.zoom(2, 2)
-    label2 = Label(window, bg="#323231",image=larger_image)
-    label2.place(relx=.5,rely=.8,anchor=S)
+    def playMusic(self):
+        songs = ["chillday.mp3", "betterdays.mp3", "blushes.mp3" ,"dream.mp3"]
+        rand = randrange(4)
+        self.music_button.destroy()
+        pygame.mixer.music.load(songs[rand])
+        pygame.mixer.music.play()
+        self.musicOn = True
+
+        self.music_button = Button(self.window, text = "Stop Music", fg="Black", highlightbackground="#323231", highlightthickness=0, width=5, height=1, command= lambda : self.stopMusic())
+        self.music_button.place(relx=.2,rely=.5,anchor=S)
+
+    def stopMusic(self):
+        self.music_button.destroy()
+        pygame.mixer.music.stop()
+        self.musicOn = False
+
+        self.music_button = Button(self.window, text = "Play Music", fg="Black", highlightbackground="#323231", highlightthickness=0, width=5, height=1, command= lambda : self.playMusic())
+        self.music_button.place(relx=.2,rely=.5,anchor=S)
+
+
+    def display(self,lives):
+        stages = ["hangman6.png", "hangman5.png", "hangman4.png", "hangman3.png",
+        "hangman2.png", "hangman1.png", "hangman0.png"]
+        
+        self.label1 = Label(self.window, text=" ".join(self.ingame_word), bg="#323231",fg="#C1436D",font=("Arial",20))
+        self.label1.place(relx=.65,rely=.5,x=900/2 - 250,anchor=CENTER)
+        
+        self.photo_image = PhotoImage(file=stages[lives]).zoom(2, 2)
+        self.label2 = Label(self.window, bg="#323231",image=self.photo_image)
+        self.label2.place(relx=.5,rely=.78,anchor=S)
+
+
+    def gameChoice(self):
+        self.canvas = Canvas(self.window, bg="#323231", highlightthickness=0)
+        self.canvas.place(relx=.45,rely=.6,anchor=S)
+        label = Label(self.canvas, text="Pick a theme", bg="#323231",fg="#C1436D",font=("Arial",35))
+        label.place(relx=.2,rely=.2)
+
+        button = Button(self.canvas, text = "Random Words", fg="Black", highlightbackground="#323231", highlightthickness=0, width=10, height=2, command= lambda : self.restart())
+        button.place(relx=.58,rely=.5)
+        button2 = Button(self.canvas, text = "US States", fg="Black", highlightbackground="#323231", highlightthickness=0, width=10, height=2, command= lambda : self.restart())
+        button2.place(relx=.58,rely=.7)
+        button4 = Button(self.canvas, text = "US Largest Cities", fg="Black", highlightbackground="#323231", highlightthickness=0, width=10, height=2, command= lambda : self.restart())
+        button4.place(relx=.1,rely=.5)
+        button5 = Button(self.canvas, text = "Countries", fg="Black", highlightbackground="#323231", highlightthickness=0, width=10, height=2, command= lambda : self.restart())
+        button5.place(relx=.1,rely=.7)
+
+    def main(self):
+        self.gameChoice()
+        self.display(self.lives)
+        
+        self.music_button = Button(self.window, text = "Play Music", fg="Black", highlightbackground="#323231", highlightthickness=0, width=5, height=1, command= lambda : self.playMusic())
+        self.music_button.place(relx=.2,rely=.5,anchor=S)
    
-    canvas = Canvas(window, bg="#323231", highlightthickness=0)
-    canvas.place(relx=.42,rely=.97,anchor=S)
-    for i in range(65,91):
-        n = chr(i)
-        n_button = Button(canvas, text=n, fg="Black", highlightbackground="#323231", width=4, height=1, command= lambda i=i, n=n: play(i, n))
-        if i < 72:
-            n_button.grid(padx=2,pady=2,row=i//9, column=i%9 - 1,)  
-        elif i == 90:
-            n_button.grid(padx=2,pady=2,row=i//9, column=4)
-        else:
-            n_button.grid(padx=2,pady=2,row=i//9, column=i%9)
-        buttons.append(n_button)
+        self.canvas = Canvas(self.window, bg="#323231", highlightthickness=0)
+        self.canvas.place(relx=.42,rely=.97,anchor=S)
+        for i in range(65,91):
+            n = chr(i)
+            n_button = Button(self.canvas, text=n, fg="Black", highlightbackground="#323231", width=4, height=1, command= lambda i=i, n=n: self.play(i - 65, n))
+            if i < 72:
+                n_button.grid(padx=2,pady=2,row=i//9, column=i%9 - 1,)  
+            elif i == 90:
+                n_button.grid(padx=2,pady=2,row=i//9, column=4)
+            else:
+                n_button.grid(padx=2,pady=2,row=i//9, column=i%9)
+            self.buttons.append(n_button)
    
-
-    label1 = Label(window, text=" ".join(ingame_word), bg="#323231",fg="#C1436D",font=("Arial",20))
-    label1.place(relx=.6,rely=.5,x=900/2 - 250,anchor=CENTER)
-    window.mainloop()
+        self.window.mainloop()
 
 
-main()
+
+game = Hangman()
+game.main()
--- words.py
+++ words.py
@@ -3,15 +3,15 @@ Bismallah
 """
 import random
 
-def get_word_list():
+def get_word_list(choice):
     words = []
-    with open("words.txt") as word_file:
+    with open(choice) as word_file:
         for line in word_file:
-            if (len(line) > 4):
+            if (len(line) >= 4):
                 words.append(line.strip())
     return words
 
-def game_word():
-    word_list = get_word_list()
+def game_word(choice):
+    word_list = get_word_list(choice)
     rand = random.randrange(len(word_list) + 1)
     return word_list[rand].upper()
